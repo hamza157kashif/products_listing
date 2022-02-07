27230db629d2366dd75b93e3b49d4f054b0d31bc
hamza157kashif <hamzakashif157@gmail.com>
2022-02-07 09:55:34 +0500

    single product page missing

--- src/controllers/productControllers.ts
+++ src/controllers/productControllers.ts
@@ -1,43 +1,127 @@
-import  Express  from "express";
+import Express, { NextFunction, response } from "express";
 import { Product } from "../enitities/Product";
 import { ProductService } from "../services/productService";
-import { Body, Get, JsonController, Param, Post, Res } from "routing-controllers";
-import { Request,Response } from "express";
-const router=Express.Router();
-let service=new ProductService();
-let msg="succesful";
+import {
+  Body,
+  ContentType,
+  Get,
+  JsonController,
+  Param,
+  Post,
+  Req,
+  Res,
+  UseBefore,
+} from "routing-controllers";
+import { Request, Response } from "express";
+import multer from "multer";
+
+//const uploadFile = require("../s3");
+const router = Express.Router();
+let service = new ProductService();
+let msg = "succesful";
+
+const upload = multer({ dest: "uploads/" });
+
+require("dotenv").config();
+const fs = require("fs");
+//import { S3 } from "aws-sdk";
+const S3 = require("aws-sdk/clients/s3");
+
+// const bucketName = process.env.AWS_BUCKET_NAME;
+// const region = process.env.AWS_BUCKET_REGION;
+// const accessKeyId = process.env.AWS_ACCESS_KEY;
+// const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const region = "us-east-1";
+const accessKeyId = "AKIAT67EESFVKELN64HN";
+const secretAccessKey = "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";
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+});
+
+// uploads a file to s3
+async function uploadFile(file: any) {
+  const fileStream = await fs.createReadStream(file.path);
+
+  const uploadParams = {
+    Bucket: "products-list-app",
+    Body: fileStream,
+    Key: file.filename,
+    Conditions: ["Content-Type", "image/jpeg"],
+  };
+  //console.log("uploadparams:", uploadParams);
+
+  return s3.upload(uploadParams).promise();
+}
 
 @JsonController()
-export class ProductController{
-   
-    @Post('/newProduct')
-    post(@Body() product:Product,@Res() response:any){
-
-        if(!service.addProduct(product)){
-            return response.json({
-                msg:"not added"
-            })
-        }
-        else{
-            return response.json({
-                msg:"added"
-            })
-        } 
+export class ProductController {
+  @Post("/newProduct")
+  post(@Body() product: Product, @Res() response: any) {
+    if (!service.addProduct(product)) {
+      return response.json({
+        msg: "not added",
+      });
+    } else {
+      return response.json({
+        msg: "added",
+      });
     }
+  }
 
-    @Get('/products')
-    async getAll(@Res() res: Response){
-        const product = await service.getProducts();
-        console.log(product);
-        return res.json(product);
+  @Post("/uploadProduct")
+  @UseBefore(upload.single("image"))
+  async postingPictures(@Req() request: any, @Res() response: any) {
+    const file = request.file;
+    //console.log(file);
+    const description = request.body.description;
+    const name = request.body.productname;
+    console.log("name:", name);
+    const result = await uploadFile(file);
+    //console.log(result);
+    console.log("done with uploading");
+
+    const url = result.Location;
+    //console.log("urllocation:", url);
+
+    //creating product object
+    const prod = Product.create({
+      name,
+      description,
+      imageURL: url,
+    });
+    prod.imageURL = prod.imageURL.concat(".jpg");
+
+    response.set("Access-Control-Allow-Origin", "*");
+
+    if (!service.addProduct(prod)) {
+      return response.json({
+        msg: "not added",
+      });
+    } else {
+      return response.json({
+        msg: "added",
+      });
     }
-    @Get('/products/:id')
-    async getone(@Param('id') id :number ,@Res() res:Response){
-        const product=await service.getSingleProduct(id);
-        //console.log(product);
-        
-        return res.json(product);
+  }
 
-    }
+  @Get("/products")
+  async getAll(@Res() res: Response) {
+    const product = await service.getProducts();
+    console.log("Fetching,", product);
 
+    res.set("Access-Control-Allow-Origin", "*");
+    return res.json(product);
+  }
+
+  @Get("/products/:id")
+  async getone(@Param("id") id: number, @Res() res: Response) {
+    const product = await service.getSingleProduct(id);
+    //console.log(product);
+
+    return res.json(product);
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,48 @@
 import { Connection, createConnection } from "typeorm";
-import  Express  from "express";
+import Express from "express";
 import { Product } from "./enitities/Product";
 import { ProductCategory } from "./enitities/ProductCategory";
 import "reflect-metadata";
 import { ProductController } from "./controllers/productControllers";
 import { createExpressServer } from "routing-controllers";
-import cors from "cors"
+import cors from "cors";
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+// const corsOptions ={
+//    origin:'*',
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
 
-const app =createExpressServer({
-    controllers:[ProductController],
+const app = createExpressServer({
+  cors: true,
+  controllers: [ProductController],
 });
-const main=async()=> {
-    try{
-        await createConnection({
-            type:"postgres",
-            host:"localhost",
-            port:5432,
-            username:"postgres",
-            password:'hamza',
-            database:"productLists",
-            entities:[Product,ProductCategory],
-            synchronize:true
-        })
+const main = async () => {
+  try {
+    await createConnection({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "hamza",
+      database: "productLists",
+      entities: [Product, ProductCategory],
+      synchronize: true,
+    });
 
-            app.use(cors(corsOptions));
-            app.use(Express.json())
-            console.log("connected to db");
+    app.use(cors);
+    app.use(Express.json());
+    console.log("connected to db");
 
-            app.listen(8080,()=>{
-                console.log("Server running at 8080");  
-                
-            })
+    app.listen(8080, () => {
+      console.log("Server running at 8080");
+    });
+  } catch (error) {
+    console.error(error);
+    console.log("sending error");
 
-      
-    }
-    catch(error){
-        console.error(error);
-        console.log('sending error');
-        
-        throw new Error("unable to connect db");
-        
-    }
-}
+    throw new Error("unable to connect db");
+  }
+};
 
-main()
+main();
